e0facdd619879bba763a133bd1218a5886f36367
Dhuha Ardha <dhuhaardha@gmail.com>
2023-08-22 19:29:01 +0700

    Create Project Folder/File

---
+++ cobaapi/src/main/java/com/restfullapi/ardha/cobaapi/CobaapiApplication.java
@@ -0,0 +1,13 @@
+package com.restfullapi.ardha.cobaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobaapiApplication.class, args);
+	}
+
+}
---
+++ cobaapi/src/test/java/com/restfullapi/ardha/cobaapi/CobaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfullapi.ardha.cobaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
